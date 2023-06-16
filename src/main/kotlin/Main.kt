e03c9fb1948a3582c24d9db24d8900694b2546fc
Ian Kariuki <ian.kariuki@tangazoletu.com>
2023-06-16 17:38:01 +0300

    commiting before making any changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,19 +3,17 @@ import player.Player
 
 fun main(args: Array<String>) {
     val game:Game = Game()
-    val name:String = "Madrigal"
-    var healthPoints:Int = 100
-    var isBlessed:Boolean = true
+
     val player = Player();
-    var auraIsVisible = isBlessed && healthPoints == 100
+    var auraIsVisible = player.isBlessed && player.healthPoints == 100
     val auraColor = if (auraIsVisible) "Green" else "Red"
-    val healthStatus = game.formatHealthStatus(healthPoints,isBlessed)
+    val healthStatus = game.formatHealthStatus(player.healthPoints,player.isBlessed)
     val javaCode : JavaCode = JavaCode();
     println(auraColor)
     player.castFireBall()
 
-    println("(AuraColor: $auraColor)"+"(Blessed: ${if (isBlessed) "YES" else "NO"})")
-    println("$name is in ${healthStatus}")
+    println("(AuraColor: $auraColor)"+"(Blessed: ${if (player.isBlessed) "YES" else "NO"})")
+    println("${player.name} is in ${healthStatus}")
     javaCode.helloThere()
     var karma = when(auraColor){
         "Green" -> 5
--- src/main/kotlin/player/Player.kt
+++ src/main/kotlin/player/Player.kt
@@ -1,7 +1,18 @@
 package player
 
 class Player {
+    var healthPoints:Int = 100
+    var isBlessed:Boolean = true
+    var name: String = "Madrigal"
+    get() = field.capitalize()
+        set(value) {
+            field = value.trim()
+        }
     fun castFireBall(numFireballs: Int=2){
         println("A glass of fireball springs to existence. (x$numFireballs)")
+    }
+
+    class Dice() {
+
     }
 }
